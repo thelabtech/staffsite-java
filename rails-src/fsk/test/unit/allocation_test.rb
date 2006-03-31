014cad156b54531755c6fb472ece4f8c5124c2a6
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-03-31 04:57:50 +0000

    Added test stubs for models
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5013 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ rails-src/fsk/test/unit/allocation_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AllocationTest < Test::Unit::TestCase
+  fixtures :allocations
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
---
+++ rails-src/fsk/test/unit/kit_category_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class KitCategoryTest < Test::Unit::TestCase
+  fixtures :kit_categories
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
