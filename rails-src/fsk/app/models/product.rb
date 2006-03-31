d99e49c278f9901a6b5d76597790cedaea6e4690
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-03-31 10:38:58 +0000

    added validation, fixed table name (oops)
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5017 e11218b7-830e-0410-a693-d1c1f8b4456b

--- rails-src/fsk/app/models/product.rb
+++ rails-src/fsk/app/models/product.rb
@@ -1,7 +1,22 @@
 class Product < ActiveRecord::Base
-  set_table_name	"fsk_LineItems"
+  set_table_name	"fsk_Products"
 
   has_many :line_items
   belongs_to :kit_category
- 
+
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_numericality_of :price
+  
+  validates_format_of :image_filename,
+    :with => %r{.+\.(gif|jpg)$}i,
+    :message => "must be a gif or jpeg file"
+  
+  protected
+  def validate
+    if price && price < 0.01
+      errors.add(:price, "should be positive") 
+    end
+  end
+  
 end
