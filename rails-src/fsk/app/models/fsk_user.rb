c2b7b80fe2e9ab8dba9d3ed6b98cc63b43cd8d0e
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-03-31 23:51:49 +0000

    Added model for fsk_user
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5029 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ rails-src/fsk/app/models/fsk_user.rb
@@ -0,0 +1,2 @@
+class FskUser < ActiveRecord::Base
+end
---
+++ rails-src/fsk/test/unit/fsk_user_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class FskUserTest < Test::Unit::TestCase
+  fixtures :fsk_users
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
