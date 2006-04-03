4ed3d60269db0de0eb961e3127c1860de1ab6db3
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-03 16:08:22 +0000

    Added delegation to ssm_user
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5033 e11218b7-830e-0410-a693-d1c1f8b4456b

--- rails-src/fsk/app/models/fsk_user.rb
+++ rails-src/fsk/app/models/fsk_user.rb
@@ -1,2 +1,12 @@
 class FskUser < ActiveRecord::Base
+	set_table_name "fsk_Users"
+	belongs_to :user, :foreign_key => :ssm_id
+		
+	# delegate all the ssm_user methods so we only have to use FskUser in the code
+	def method_missing(method)
+		user[method]
+	end
+	def emailVerified=(value)
+	   user.emailVerified = value
+	end
 end
