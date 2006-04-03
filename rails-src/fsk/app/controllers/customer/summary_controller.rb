19a85a2092e079fbb59c267344bedbaa12d0f797
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-03 18:18:00 +0000

    Structure authorization something like this, maybe
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5034 e11218b7-830e-0410-a693-d1c1f8b4456b

--- rails-src/fsk/app/controllers/customer/summary_controller.rb
+++ rails-src/fsk/app/controllers/customer/summary_controller.rb
@@ -1,2 +1,33 @@
 class Customer::SummaryController < ApplicationController
+  
+  def index
+    if get_user_role == "national"
+      return national_summary
+    end
+    if get_user_role == "regional"
+      return regional_summary
+    end
+    if get_user_role == "local"
+      return local_summary
+    end
+    #error
+  end
+  
+  def national_summary
+    #assert get_user_role == "national"
+    
+  end
+  
+  def regional_summary
+    #assert get_user_role == "regional"
+    
+  end
+  
+  def local_summary
+    #assert get_user_role == "regional"
+    
+  end
+  
+  
+  
 end
