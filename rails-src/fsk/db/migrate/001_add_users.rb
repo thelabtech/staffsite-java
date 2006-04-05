2a2843316f09075be1c83c375fad5c6d7eeff38d
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-05 18:13:01 +0000

    tweak the ssm table and add a couple rows
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5052 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ rails-src/fsk/db/migrate/001_add_users.rb
@@ -0,0 +1,22 @@
+class AddUsers < ActiveRecord::Migration
+  def self.up
+    #rename_column("simplesecuritymanager_user", "email", "email_deprecated")
+    change_column("simplesecuritymanager_user", "lastFailure", :datetime, :null => true)
+    execute "INSERT INTO simplesecuritymanager_user (userID, globallyUniqueID, username, password, "+
+                                                    "passwordQuestion, passwordAnswer, lastFailure, lastFailureCnt, "+
+                                                    "lastLogin, createdOn, emailVerified) "+
+                                            "VALUES (42655,'F167605D-94A4-7121-2A58-8D0F2CA6E026','josh.starcher@uscm.org', "+
+                                            "        'DomB6DfPieMK1vx6Tt6Vpw==', 'why?', '42', NULL, 0, '2006-04-04', "+
+                                            "       '2004-04-09', 1)"
+    execute "INSERT INTO simplesecuritymanager_user (userID, globallyUniqueID, username, password, "+
+                                                    "passwordQuestion, passwordAnswer, lastFailure, lastFailureCnt, "+
+                                                    "lastLogin, createdOn, emailVerified) "+
+                                            "VALUES (58616, 'F167605D-94A4-7121-2A58-8D0F2CA6E026','matthew.drees@uscm.org', "+
+                                            "        'P+vaORoZmwI2wP29FLvcyw==', 'stupid?', 'yes', NULL, 0, '2006-04-05', "+
+                                            "       '2005-05-09', 1)"
+  end
+
+  def self.down
+    rename_column("simplesecuritymanager_user", "email_deprecated", "email")
+  end
+end
