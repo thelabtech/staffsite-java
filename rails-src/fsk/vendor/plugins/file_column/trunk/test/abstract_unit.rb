67654e49411f3cb79f6ba6bde79a70bc1675d696
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-05 16:01:25 +0000

    Added file_column plugin to the common_engine because it is used by the Person class
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5048 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ rails-src/fsk/vendor/plugins/file_column/trunk/test/abstract_unit.rb
@@ -0,0 +1,90 @@
+require 'test/unit'
+require 'rubygems'
+require 'active_support'
+require 'active_record'
+require 'action_view'
+require File.dirname(__FILE__) + '/connection'
+require 'stringio'
+
+RAILS_ROOT = File.dirname(__FILE__)
+
+$: << "../lib"
+
+require 'file_column'
+require 'file_compat'
+require 'validations'
+
+# do not use the file executable normally in our tests as
+# it may not be present on the machine we are running on
+FileColumn::ClassMethods::DEFAULT_OPTIONS = 
+  FileColumn::ClassMethods::DEFAULT_OPTIONS.merge({:file_exec => nil})
+
+class ActiveRecord::Base
+    include FileColumn
+    include FileColumn::Validations
+end
+
+
+class RequestMock
+  attr_accessor :relative_url_root
+
+  def initialize
+    @relative_url_root = ""
+  end
+end
+
+class Test::Unit::TestCase
+  def assert_equal_paths(expected_path, path)
+    assert_equal normalize_path(expected_path), normalize_path(path)
+  end
+
+
+  private
+  
+  def normalize_path(path)
+    Pathname.new(path).realpath
+  end
+
+  def uploaded_file(path, content_type, filename, type=:tempfile)
+    if type == :tempfile
+      t = Tempfile.new(File.basename(filename))
+      FileUtils.copy_file(path, t.path)
+    else
+      if path
+        t = StringIO.new(IO.read(path))
+      else
+        t = StringIO.new
+      end
+    end
+    (class << t; self; end).class_eval do
+      alias local_path path if type == :tempfile
+      define_method(:local_path) { "" } if type == :stringio
+      define_method(:original_filename) {filename}
+      define_method(:content_type) {content_type}
+    end
+    return t
+  end
+
+  def upload(basename, content_type=:guess, file_type=:tempfile)
+    if content_type == :guess
+      case basename
+        when /\.jpg$/ then content_type = "image/jpeg"
+        when /\.png$/ then content_type = "image/png"
+        else content_type = nil
+      end
+    end
+    uploaded_file(file_path(basename), content_type, basename, file_type)
+  end
+  
+  def clear_validations
+    [:validate, :validate_on_create, :validate_on_update].each do |attr|
+        Entry.write_inheritable_attribute attr, []
+        Movie.write_inheritable_attribute attr, []
+      end
+  end
+
+  def file_path(filename)
+    File.expand_path("#{File.dirname(__FILE__)}/fixtures/#{filename}")
+  end
+
+end
