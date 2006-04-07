4b6a92edd19a327ef9a7d32cbc8818db065bd43e
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-07 21:55:53 +0000

    passes tests
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5090 e11218b7-830e-0410-a693-d1c1f8b4456b

--- rails-src/fsk/app/models/report_record.rb
+++ rails-src/fsk/app/models/report_record.rb
@@ -3,17 +3,16 @@ class ReportRecord < ActiveRecord::Base
   #some ugly sql statement to count orders...
   @@sql = 
 "SELECT     alloc.*," +
-"  (SELECT     SUM(fsk_FskOrder.totalKits)" +
-"  FROM          fsk_FskOrder" +
-"  WHERE      alloc.AllocationID = fsk_FskOrder.fk_Allocation) AS ordered_kits" +
-"FROM         fsk_Allocation alloc"
+"  (SELECT     SUM(fsk_orders.total_kits)" +
+"  FROM          fsk_orders" +
+"  WHERE      alloc.ssm_id = fsk_orders.ssm_id and fsk_orders.type = 'kit' ) AS ordered_kits" +
+" FROM         fsk_allocations alloc"
 
-
-  def records_for_region(region)
-    find_by_sql(@@sql + " WHERE region = " + region)
+  def self.records_for_region(region)
+    find_by_sql(@@sql + ", ministry_RegionalTeam region WHERE region_id = region.teamID and region.region = '" + region + "'")
   end
   
-  def all_records()
+  def self.all_records()
     find_by_sql(@@sql)
   end
   
--- rails-src/fsk/test/unit/report_record_test.rb
+++ rails-src/fsk/test/unit/report_record_test.rb
@@ -1,12 +1,16 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ReportRecordTest < Test::Unit::TestCase
-  fixtures :fsk_allocations, :fsk_orders
+  fixtures :fsk_allocations, :fsk_orders, :ministry_RegionalTeam
 
   # Replace this with your real tests.
   def test_records_for_region
     report_records = ReportRecord.records_for_region("GL")
     assert report_records.size > 0
-    
+  end
+  
+  def test_all_records
+    report_records = ReportRecord.all_records
+    assert report_records.size > 0
   end
 end
