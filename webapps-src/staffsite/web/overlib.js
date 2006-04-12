8ac74562c2aeed7793c3d973a00d6c8b681400f4
scott.welch <scott.welch@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-12 14:00:05 +0000

    Changed border color back to #336699 and changed background color to yellow
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5127 e11218b7-830e-0410-a693-d1c1f8b4456b

--- webapps-src/staffsite/web/overlib.js
+++ webapps-src/staffsite/web/overlib.js
@@ -36,8 +36,8 @@ registerCommands('donothing,inarray,caparray,sticky,background,noclose,caption,l
 // Settings you want everywhere are set here. All of this can also be
 // changed on your html page or through an overLIB call.
 ////////
-if (typeof ol_fgcolor=='undefined') var ol_fgcolor="#CCCC99";
-if (typeof ol_bgcolor=='undefined') var ol_bgcolor="yellow";
+if (typeof ol_fgcolor=='undefined') var ol_fgcolor="yellow";
+if (typeof ol_bgcolor=='undefined') var ol_bgcolor="#336699";
 if (typeof ol_textcolor=='undefined') var ol_textcolor="#336699";
 if (typeof ol_capcolor=='undefined') var ol_capcolor="#FFFFFF";
 if (typeof ol_closecolor=='undefined') var ol_closecolor="#9999FF";
