e2421039048b8691273e4d8739f88945a6072094
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-17 20:56:16 +0000

    validation
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5156 e11218b7-830e-0410-a693-d1c1f8b4456b

--- rails-src/fsk/app/models/allocation.rb
+++ rails-src/fsk/app/models/allocation.rb
@@ -4,11 +4,48 @@ class Allocation < ActiveRecord::Base
   belongs_to :user, :foreign_key => "ssm_id"
   belongs_to :region
 
+ 
+  validates_numericality_of :impact_allotment
+  validates_numericality_of :forerunner_allotment
+  validates_numericality_of :regional_allotment
+  validates_numericality_of :regionally_raised
+  validates_numericality_of :locally_raised
+ 
+  
+  def after_initialize
+    self.impact_allotment ||= 0
+    self.forerunner_allotment ||= 0
+    self.regional_allotment ||= 0
+    self.regionally_raised ||= 0
+    self.locally_raised ||= 0
+  end
+
   def total_kits
     (impact_allotment || 0) + 
     (forerunner_allotment || 0) +
     (regional_allotment || 0) +
     (regionally_raised || 0) +
     (locally_raised || 0)
+  end
+  
+  protected
+  def validate
+    message = "must be multiples of 100."
+    if (impact_allotment % 100) != 0
+      errors.add(:impact_allotment, message)
+    end
+    if (forerunner_allotment % 100) != 0
+      errors.add(:forerunner_allotment, message)
+    end
+    if (regional_allotment % 100) != 0
+      errors.add(:regional_allotment, message)
+    end
+    if (regionally_raised % 100) != 0
+      errors.add(:regionally_raised, message)
+    end
+    if (locally_raised % 100) != 0
+      errors.add(:locally_raised, message)
+    end
+    super
   end
 end
