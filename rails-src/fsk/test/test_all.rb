5834c5de8153956f49d07b5282ce462a7778ab88
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-19 15:56:30 +0000

    file for all the tests
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5185 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ rails-src/fsk/test/test_all.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+
+#Unit tests.
+require 'unit/fsk_user_test.rb'
+require 'unit/kit_category_test.rb'
+require 'unit/line_item_test.rb'
+require 'unit/order_test.rb'
+require 'unit/kit_order_test.rb'
+require 'unit/product_order_test.rb'
+
+#Functional tests.
