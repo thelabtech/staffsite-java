ec413e6041d9b67c5b958270736ea31253c1bc13
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-04-28 12:39:18 +0000

    set primary key
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5279 e11218b7-830e-0410-a693-d1c1f8b4456b

--- common-rails-src/common_engine/app/models/ministry_activity.rb
+++ common-rails-src/common_engine/app/models/ministry_activity.rb
@@ -1,5 +1,6 @@
 class MinistryActivity < ActiveRecord::Base
   set_table_name			"ministry_Activity"
+  set_primary_key   			"ActivityID"
   belongs_to :target_area, :foreign_key => "fk_targetAreaID"
   
 end
