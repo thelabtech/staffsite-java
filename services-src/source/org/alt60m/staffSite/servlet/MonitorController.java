0e6ee05cf89eb8fc935b34d5fe9f2c712e3cb3f0
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-06-05 22:00:20 +0000

    A new page for IP sentry to monitor
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5356 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ services-src/source/org/alt60m/staffSite/servlet/MonitorController.java
@@ -0,0 +1,153 @@
+package org.alt60m.staffSite.servlet;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alt60m.ministry.model.dbio.Person;
+import org.alt60m.ministry.model.dbio.Staff;
+import org.alt60m.security.dbio.model.User;
+import org.alt60m.servlet.Controller;
+
+import com.kenburcham.framework.dbio.DBIOEntity;
+
+public class MonitorController extends Controller {
+	
+	public void getStatus(ActionContext ctx)
+	{
+		String requester = ctx.getInputString("requester");
+		if (requester != null)
+			log.info("Status page requested by " + requester);
+		else
+			log.info("Status page reqeusted anonymously");
+		
+		Map<String, String> results = new HashMap<String, String>();
+		//check some tables
+		
+		boolean dbFailure = testDatabase(results);
+
+		//check gcx
+		
+		boolean gcxFailure = testGCX(results);
+		
+		//check cas
+		
+		boolean casFailure = testCAS(results);
+		
+		boolean failure = dbFailure || gcxFailure || casFailure;
+		String statusMessage = (failure ? "FAIL" : "OK");
+		results.put("statusMessage", statusMessage);
+		
+		ctx.setReturnValue(results);
+		ctx.goToURL("/monitor.jsp");
+	}
+
+	private boolean testDatabase(Map<String, String> results) {
+		//Ministry Person
+		long testStartTime = System.currentTimeMillis();
+		Person testPerson = new Person();
+		testPerson.setToolName("monitor");
+		testPerson.setFirstName("Test");
+		testPerson.setCreatedBy("monitor");
+		testPerson.setDateCreated(new Date());
+		String updateFieldName = "FirstName";
+		Object updateValue = "Test2";
+
+		boolean personFailure = testEntity(testPerson, updateFieldName, updateValue);
+			
+		long testTime = System.currentTimeMillis() - testStartTime;
+		String personResult = "Person test completed in " + testTime + " ms; result: " + (personFailure ? "failure" : "success"); 
+		log.info(personResult);
+		results.put("personResult", personResult);
+
+		//SSM User
+		testStartTime = System.currentTimeMillis();
+		User testUser = new User();
+		testUser.setUsername("Test.User@test.org");
+		testUser.setPassword("testpassword");
+		testUser.setCreatedOn(new Date());
+		updateFieldName = "Password";
+		updateValue = "testpassword2";
+
+		boolean userFailure = testEntity(testUser, updateFieldName, updateValue);
+			
+		testTime = System.currentTimeMillis() - testStartTime;
+		String userResult = "SSM User test completed in " + testTime + " ms; result: " + (userFailure ? "failure" : "success"); 
+		log.info(userResult);
+		
+		results.put("userResult", userResult);
+		
+//		Ministry Staff
+		/* turns out DBIO can't do inserts with non-database assigned IDs
+		testStartTime = System.currentTimeMillis();
+		Staff testStaff = new Staff();
+		testStaff.setFirstName("Test");
+		testStaff.setLastName("Staff");
+		updateFieldName = "LastName";
+		updateValue = "Test2";
+
+		boolean staffFailure = testEntity(testStaff, updateFieldName, updateValue);
+			
+		testTime = System.currentTimeMillis() - testStartTime;
+		String staffResult = "Staff test completed in " + testTime + " ms; result: " + (staffFailure ? "failure" : "success"); 
+		log.info(staffResult);
+		results.put("staffResult", staffResult);
+		*/
+		
+		boolean dbFailure = personFailure || userFailure; // || staffFailure;
+		String dbMessage = (dbFailure ? "FAIL" : "OK");
+		results.put("dbMessage", dbMessage);
+		return dbFailure;
+	}
+
+	private boolean testEntity(DBIOEntity entity, String updateFieldName, Object updateValue) {
+		boolean failure = false;
+		boolean successfulInsert = entity.insert();
+		if (successfulInsert) {
+			if (entity.isPKEmpty()) {
+				log.error("Test object received no id on successful insertion!");
+				failure = true;
+			}
+			log.info("Successful insert");
+			entity.setFieldValue(updateFieldName, updateValue);
+			boolean successfulUpdate = entity.persist();
+			if (successfulUpdate) {
+				log.info("Successful update");
+			}
+			else {
+				log.warn("Unsuccessful update");
+				failure = true;
+			}
+			boolean successfulDelete = entity.delete();
+			if (successfulDelete) {
+				log.info("Successful delete");
+			}
+			else {
+				log.warn("Unsuccessful delete");
+				failure = true;
+			}
+		}
+		else {
+			log.warn("Unsucessful insert");
+			failure = true;
+		}
+		return failure;
+	}
+	
+
+	private boolean testGCX(Map<String, String> results) {
+		
+		boolean gcxFailure = false;
+		String gcxMessage = (gcxFailure ? "FAIL" : "OK");
+		results.put("gcxMessage", gcxMessage);
+		return gcxFailure;
+	}
+	
+	private boolean testCAS(Map<String, String> results) {
+		
+		boolean casFailure = false;
+		String casMessage = (casFailure ? "FAIL" : "OK");
+		results.put("casMessage", casMessage);
+		return casFailure;
+	}
+}
---
+++ webapps-src/staffsite/web/monitor.jsp
@@ -0,0 +1,60 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page isELIgnored="false" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Staffsite Status Monitor</title>
+</head>
+<body>
+<h1>
+Staffsite Status
+</h1>
+
+<h3>
+Status: ${tub.statusMessage }
+</h3>
+
+<h3>
+Database Tests:
+</h3>
+<p>
+Status: ${tub.dbMessage }
+</p>
+<p>
+Person results:
+</br>
+${tub.personResult }
+</p>
+
+<p>
+User results:
+</br>
+${tub.userResult }
+</p>
+<%-- 
+<p>
+Staff results:
+</br>
+${tub.staffResult }
+</p>
+--%>
+<h3>
+GCX Tests:
+</h3>
+<p>
+Status: (not yet implemented)
+</p>
+
+<h3>
+CAS Tests:
+</h3>
+<p>
+Status: (not yet implemented)
+</p>
+
+
+
+</body>
+</html>
