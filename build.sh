ef1d55ac35dd2af3cdfc993ceb2fbe092dd30eeb
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-06-15 23:14:31 +0000

    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5384 e11218b7-830e-0410-a693-d1c1f8b4456b

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 JAVA_HOME=/usr/java/jdk
 export JAVA_HOME
 ant -buildfile $1
-cp -R /var/www/html/integration/staffsite/current/config/* /var/www/html/integration/staffsite/current/web
+cp -Rf /var/www/html/integration/staffsite/current/config/* /var/www/html/integration/staffsite/current/web
