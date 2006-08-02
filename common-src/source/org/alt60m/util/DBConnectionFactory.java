eee29d087a2f11837d8e8455bdf1487f49f60c32
matt.drees <matt.drees@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-08-02 13:57:19 +0000

    updated oracle connection info
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5552 e11218b7-830e-0410-a693-d1c1f8b4456b

--- common-src/source/org/alt60m/util/DBConnectionFactory.java
+++ common-src/source/org/alt60m/util/DBConnectionFactory.java
@@ -19,8 +19,8 @@ public class DBConnectionFactory {
 
 	protected static Log log = LogFactory.getLog(DBConnectionFactory.class);
 	
-	private static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private static final String ORACLE_PROD_URL = "jdbc:oracle:thin:@(description=(address=(host=hart-ca803v)(protocol=tcp)(port=1526))(connect_data=(sid=prod)))";
+	private static final String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
+    private static final String ORACLE_PROD_URL = "jdbc:oracle:thin:@hart-a811v.ccci.org:1521:prod2";
 	private static final String ORACLE_DEFAULT_USER = "istcampus";
 	private static final String ORACLE_DEFAULT_PW = "gocampus";
 
