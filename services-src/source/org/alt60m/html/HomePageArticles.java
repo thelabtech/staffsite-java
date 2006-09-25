065289f54856c8bce238b9c3ed5013f83551d8d6
dan.davies <dan.davies@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-09-25 21:05:18 +0000

    Trying again to get it to display 2 articles instead of one.
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5596 e11218b7-830e-0410-a693-d1c1f8b4456b

--- services-src/source/org/alt60m/html/HomePageArticles.java
+++ services-src/source/org/alt60m/html/HomePageArticles.java
@@ -100,7 +100,7 @@ public class HomePageArticles implements java.io.Serializable {
 					File recent = (File) iter.next();
 					
 					//print out the rest of the headlines without the summary
-					if(i != 0 || 1) {
+					if(i < 2) {
 						
 						if(i == 2) {
 							stringBuffer = stringBuffer + "				<br>"+ authorFont + "More Headlines:" + "</font>";
