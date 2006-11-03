e60b337c1a9d0e8e58d9bcf4ef0478fe86db7ce1
josh.starcher <josh.starcher@e11218b7-830e-0410-a693-d1c1f8b4456b>
2006-11-03 07:16:07 +0000

    Make generated password just a little bit prettier.
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5672 e11218b7-830e-0410-a693-d1c1f8b4456b

--- services-src/source/org/alt60m/accounts/servlet/dbio/AccountController.java
+++ services-src/source/org/alt60m/accounts/servlet/dbio/AccountController.java
@@ -634,7 +634,7 @@ public class AccountController extends org.alt60m.servlet.Controller {
 	}
 
 	private String generatePassword(int passLength) {
-		char[] chars = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','`','~','!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','|',';',':',',','.','?' };
+		char[] chars = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!','$','?' };
 		int _CHARS_ARRAY_SIZE = chars.length;
 		String newPassword = "";
 		for (int i = 0; i < passLength; i++) {
