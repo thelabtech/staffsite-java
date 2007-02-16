e74d2cbcd2b376ea69b04202b872f04b76664b22
mark.sutherlin <mark.sutherlin@e11218b7-830e-0410-a693-d1c1f8b4456b>
2007-02-16 17:10:43 +0000

    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5820 e11218b7-830e-0410-a693-d1c1f8b4456b

--- services-src/source/org/alt60m/html/HomePageArticles.java
+++ services-src/source/org/alt60m/html/HomePageArticles.java
@@ -126,7 +126,7 @@ public class HomePageArticles implements java.io.Serializable {
 						stringBuffer = stringBuffer + "				<br>\n"+ authorFont + "by " + recent.getAuthor() + "</font>";
 						stringBuffer = stringBuffer + "				<br>\n<i>" + dateFont + recent.getDateAdded() + "</font></i>";
 						stringBuffer = stringBuffer + "				<br>\n" + bodyFont + recent.getSummary() + "</font>";
-						stringBuffer = stringBuffer + "				<br>\n<i><A HREF='/lx_frame.jsp?id=" + recent.getFileId() + "' target='_blank'><font size=1 face=arial color='#336699'>Full Story</font></a></i><br>";
+						stringBuffer = stringBuffer + "				<br>\n<i><A HREF='/lx_frame.jsp?id=" + recent.getFileId() + "' target='_blank'><font size=1 face=arial color='#336699'>Full Story</font></a></i><p>";
 						
 					}
 				
