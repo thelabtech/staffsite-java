751ee217d3040a79bb6cb1a925d151da0fe79570
justin.sabelko <justin.sabelko@e11218b7-830e-0410-a693-d1c1f8b4456b>
2007-09-04 19:57:25 +0000

    Fix compile error
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5911 e11218b7-830e-0410-a693-d1c1f8b4456b

--- services-src/source/org/alt60m/html/HomePageArticles.java
+++ services-src/source/org/alt60m/html/HomePageArticles.java
@@ -125,7 +125,7 @@ public class HomePageArticles implements java.io.Serializable {
 						stringBuffer = stringBuffer + "			<b>" + titleFont + recent.getTitle() + "</font></b>";
 						stringBuffer = stringBuffer + "				<br>\n"+ authorFont + "by " + recent.getAuthor() + "</font>";
 					//	stringBuffer = stringBuffer + "				<br>\n<i>" + dateFont + recent.getDateAdded() + "</font></i>"; //
-						stringBuffer = stringBuffer + "				<br>\n" + bodyFont + recent.getSummary()"</font>";
+						stringBuffer = stringBuffer + "				<br>\n" + bodyFont + recent.getSummary() + "</font>";
 					}
 				
 				}
