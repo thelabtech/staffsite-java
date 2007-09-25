20707d41bfda608a8ac4e618d2a2d194c1dafc39
justin.sabelko <justin.sabelko@e11218b7-830e-0410-a693-d1c1f8b4456b>
2007-09-25 16:05:44 +0000

    Added error logging to the CRS campus locator
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@5947 e11218b7-830e-0410-a693-d1c1f8b4456b

--- services-src/source/org/alt60m/crs/servlet/CRSRegister.java
+++ services-src/source/org/alt60m/crs/servlet/CRSRegister.java
@@ -2387,6 +2387,7 @@ public class CRSRegister extends org.alt60m.servlet.Controller {
 			String searchText = ctx.getInputString("searchtext", true);
 			outputCampusList(ctx.getResponse(), searchText);
 		} catch (Exception e) {
+			log.error("Failed to perform campusLocate", e);
 			goToErrorPage(ctx, e, "campusLocate");
 		}
 	}
