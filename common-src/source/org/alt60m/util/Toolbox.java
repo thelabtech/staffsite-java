e973c92f7666548b01fa0f99bf3e5b9512e66c02
isaac.kimball <isaac.kimball@e11218b7-830e-0410-a693-d1c1f8b4456b>
2008-04-02 19:03:38 +0000

    helpful tools for string and integer modification
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@6068 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ common-src/source/org/alt60m/util/Toolbox.java
@@ -0,0 +1,32 @@
+package org.alt60m.util;
+public final class Toolbox
+{
+	
+	//Trims surrounding whitespace, then trailing non-numeric characters, from a String representation
+	//of an Integer. If the non-whitespace portion starts with any non-numeric character, it returns null.
+	// also returns null if there is no integer to be found.
+	//the value "fox" will produce null, as will "abc123". "123abc" and "123" will both produce (String)"123".
+	public static String cleanNumeric(String numericParameter){
+		String retValue=new String("");
+		if (numericParameter!=null){
+			retValue=numericParameter;
+			}
+		return retValue.trim().replaceAll("[^-0123456789]","a").split("a",1)[0];
+	}
+	//converts a String to an Integer without throwing an exception on an invalid input value.
+	//the value "fox" will produce null, as will "abc123". "123abc" and "123" will both produce (Integer)123.
+	public static Integer stringToInteger(String numeric){
+		String numberMaybe=cleanNumeric(numeric);
+		if (numberMaybe!=null){
+			return Integer.valueOf(numberMaybe);
+		}
+		else 
+		{
+			return null;
+		}
+	}
+	public static String escapeSingleQuotes(String fixme){
+		return fixme.replace("'","%27");
+	}
+	
+}
