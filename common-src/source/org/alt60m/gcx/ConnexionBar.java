a95b4bdf2533c100dec207bf18622461467caa3d
justin.sabelko <justin.sabelko@e11218b7-830e-0410-a693-d1c1f8b4456b>
2008-05-03 16:28:57 +0000

    New Connexion Bar URLs
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@6096 e11218b7-830e-0410-a693-d1c1f8b4456b

--- common-src/source/org/alt60m/gcx/ConnexionBar.java
+++ common-src/source/org/alt60m/gcx/ConnexionBar.java
@@ -111,8 +111,8 @@ public class ConnexionBar {
 
 		String content = null;
 		//TODO: at some point, the GCX guys need to fix their system so we can request a ticket for the same URL we use to get the bar itself
-		String barTicketService = "http://www.mygcx.org/module/CampusStaff/omnibar/omnibar";
-		String barService = "http://gcx3.mygcx.org/module/CampusStaff/omnibar/omnibar"; //should be www, not working
+		String barTicketService = "https://www.mygcx.org/module/CampusStaff/omnibar/omnibar";
+		String barService = "https://www.mygcx.org/module/CampusStaff/omnibar/omnibar"; //should be www, not working
 //		String barTicketService = "http://www.mygcx.org/module/global/omnibar/omnibarExternal";
 //		String barService = "http://gcx3.mygcx.org/module/global/omnibar/omnibarExternal";
 		// "http://gcx1.mygcx.org/module/global/omnibar/omnibarExternal";
