8553aa26de3cb04baee5989adcfb4c3a83f324b5
isaac.kimball <isaac.kimball@e11218b7-830e-0410-a693-d1c1f8b4456b>
2009-02-25 16:47:32 +0000

    testing code for Oracle connexion for StaffUpdater
    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@6313 e11218b7-830e-0410-a693-d1c1f8b4456b

---
+++ services-src/source/org/alt60m/ministry/servlet/TestOracleController.java
@@ -0,0 +1,31 @@
+package org.alt60m.ministry.servlet;
+
+import java.util.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import org.alt60m.servlet.*;
+import org.alt60m.ministry.servlet.StaffInfo;
+import org.alt60m.ministry.model.dbio.*;
+
+import org.apache.log4j.*;
+import org.alt60m.util.*;
+import org.alt60m.staffSite.bean.dbio.*;
+import org.alt60m.staffSite.profiles.dbio.*;
+import org.alt60m.staffSite.model.dbio.*;
+
+/**
+ * Staff Controller
+ *		5/14/01 - Refactored to Controller2 by MDP
+ *
+ * @author Mat Weiss
+ *
+ */
+public class TestOracleController extends Controller {
+
+	
+	public void testStaffUpdater(ActionContext ctx){
+		org.alt60m.ministry.bean.TestStaffUpdater tsu = new org.alt60m.ministry.bean.TestStaffUpdater();
+		tsu.performUpdate();
+		ctx.goToURL("/servlet/StaffController?action=showHome");
+	}
+}
