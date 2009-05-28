03cec6d9fa5053ae35fdae6e884de3cc481e5809
isaac.kimball <isaac.kimball@e11218b7-830e-0410-a693-d1c1f8b4456b>
2009-05-28 20:07:01 +0000

    git-svn-id: http://svn.uscm.org/staffsite/trunk/controlled-src@6358 e11218b7-830e-0410-a693-d1c1f8b4456b

--- services-src/source/org/alt60m/ministry/Regions.java
+++ services-src/source/org/alt60m/ministry/Regions.java
@@ -11,12 +11,14 @@ public class Regions {
 	// User-readable string
 	public static final String[] abbrevRegion = new String[] {"NE", "MA", "MS", "SE", "GL", "UM", "GP", "RR", "NW", "SW","NC"};
 	public static final String[] expandedRegion = new String[] {"Northeast", "Mid-Atlantic", "MidSouth", "Southeast", "Great Lakes", "Upper Midwest", "Great Plains Int'l", "Red River", "Greater Northwest", "Pacific Southwest","National Campus Office"};
-
+	public static final String[] ps_region = new String[] {"CNE","CMIDA","CMIDS","CSE","CGL","CUPM","CWC","CRR","CGN","CPS","CMNCO"};
 
 	public static String expandRegion(String abbrev) {
 		return org.alt60m.util.TextUtils.translate(abbrevRegion, expandedRegion, abbrev);
 	}
-
+	public static String psRegionToCampusRegion(String ps) {
+		return org.alt60m.util.TextUtils.translate(ps_region, abbrevRegion, ps);
+	}
 
 	public static void main(String[] args)
 	{
