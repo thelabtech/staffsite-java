c9d6af2676a9d65fca0f5947816d35764eaae974
Justin Sabelko <justin.sabelko@uscm.org>
2010-02-18 14:59:06 -0500

    New Oracle Connection String (again)

--- common-src/source/org/alt60m/util/DBConnectionFactory.java
+++ common-src/source/org/alt60m/util/DBConnectionFactory.java
@@ -20,7 +20,7 @@ public class DBConnectionFactory {
 	protected static Log log = LogFactory.getLog(DBConnectionFactory.class);
 
 	private static final String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
-    private static final String ORACLE_PROD_URL = "jdbc:oracle:thin:@(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = hart-a8211v)(PORT = 1521)) (ADDRESS = (PROTOCOL = TCP)(HOST = hart-a8411v)(PORT = 1521)) (FAILOVER = ON) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = psbatch.ccci.org) (FAILOVER_MODE = (TYPE = SELECT) (METHOD = BASIC) (RETRIES = 180) (DELAY = 5) ) ) )";
+    private static final String ORACLE_PROD_URL = "jdbc:oracle:thin:@(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = harta8211v)(PORT = 1521)) (ADDRESS = (PROTOCOL = TCP)(HOST = harta8311v)(PORT = 1521)) (ADDRESS = (PROTOCOL = TCP)(HOST = harta8411v)(PORT = 1521)) (FAILOVER = ON) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = psbatch.ccci.org) (FAILOVER_MODE = (TYPE = SELECT) (METHOD = BASIC) (RETRIES = 180) (DELAY = 5) ) ) )";
 	private static final String ORACLE_DEFAULT_USER = "istcampus";
 	private static final String ORACLE_DEFAULT_PW = "gocampus";
 
