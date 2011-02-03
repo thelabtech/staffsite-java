dbe5f4a135ede3cb55d4aa73b065086fcc2b78e3
Justin Sabelko <justin.sabelko@uscm.org>
2011-02-03 17:05:58 -0500

    Fix account balance updater

--- services-src/source/org/alt60m/staffSite/bean/dbio/AccountBalanceUpdater.java
+++ services-src/source/org/alt60m/staffSite/bean/dbio/AccountBalanceUpdater.java
@@ -93,7 +93,7 @@ public class AccountBalanceUpdater {
 			
 	String query = "select lm.emplid as emplid, (lm.last_month_bal + nvl(tm.dasum,0)) as cur_bal from " + 
 					"(select emplid, last_month_bal from staff_last_bal_vw) lm left join (select emplid, sum(trans_amount) as dasum from SYSADM.PS_STAFF_TRNSACTNS a " +
-					"where a.stf_acct_type = 'PRIME' and a.trans_date > '01-may-2010' and a.posted_flag = 'N' and a.source_code_type in ('D','T','G') group by emplid) tm " + 
+					"where a.stf_acct_type = 'PRIME' and a.trans_date > '01-may-2010' and a.posted_flag = 'N' group by emplid) tm " + 
 					"on lm.emplid = tm.emplid order by lm.emplid";
 
 		Statement statement = m_connection.createStatement();
